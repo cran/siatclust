20e399f7110d23b7bd0b1784657ff707e97f12bb
Graham Williams <Graham.Williams@togaware.com>
2011-08-04 00:00:00 +0000

    version 1.0.3

--- R/ewkm.R
+++ R/ewkm.R
@@ -37,14 +37,22 @@ ewkm <- function(x, k, lambda=1, maxiter=100, delta=0.00001, maxrestart=10)
   weights <- matrix(Z$weights, ncol=ncol(x))
   colnames(weights) <- vars
 
-  # Identify missing clusters to be removed
+  # Identify missing clusters to be removed.  110804 Deal with the
+  # case whereby a single cluster is returned. Previous version did it
+  # properly in that centers[-ignore,] returns a matrix. But now it
+  # returns a vector. So need to use drop=FALSE
   
   ignore <- which(rowSums(centers==0) == ncol(centers))
   if (length(ignore))
   {
-    centers <- centers[-ignore,]
-    weights <- weights[-ignore,]
+    centers <- centers[-ignore,, drop=FALSE]
+    weights <- weights[-ignore,, drop=FALSE]
   }
+
+  # Give the rows names.
+
+  rownames(centers) <- 1:nrow(centers)
+  rownames(weights) <- 1:nrow(weights)
 
   cluster <- Z$cluster + 1
   
@@ -61,8 +69,61 @@ ewkm <- function(x, k, lambda=1, maxiter=100, delta=0.00001, maxrestart=10)
                  restarts=Z$restarts,
                  weights=weights)
 
-  class(result) <- "kmeans"
+  class(result) <- c("kmeans", "ewkm")
 
   return(result)
 #  return(Z)
 }
+
+plot.ewkm <- function(x, type="lattice", ...)
+{
+  if (type == "standard")
+    plot.ewkm.standard(x)
+  else
+    plot.ewkm.lattice(x)
+}
+
+
+plot.ewkm.standard <- function(x)
+{
+  x  <- t(x$weights)
+  rc <- rainbow(nrow(x), start=0, end=.3)
+  cc <- rainbow(ncol(x), start=0, end=.3)
+  hv <- heatmap(x, col = cm.colors(256), scale="column",
+                RowSideColors = rc, ColSideColors = cc, margin=c(5,10),
+                xlab = "Cluster")
+}
+
+plot.ewkm.lattice <- function(x)
+{
+  require(lattice)
+  require(latticeExtra)
+ 
+  x  <- x$weights
+  dd.row <- as.dendrogram(hclust(dist(x)))
+  row.ord <- order.dendrogram(dd.row)
+ 
+  dd.col <- as.dendrogram(hclust(dist(t(x))))
+  col.ord <- order.dendrogram(dd.col)
+
+  my.colors <- function(n) rev(heat.colors(n))
+  
+  levelplot(x[row.ord, col.ord],
+            aspect = "fill", pretty=TRUE,
+            xlab="Cluster", ylab="",
+            colorkey = list(space = "left", col=my.colors),
+            col.regions=my.colors,
+            legend =
+            list(right =
+                 list(fun = dendrogramGrob,
+                      args =
+                      list(x = dd.col, ord = col.ord,
+                           side = "right",
+                           size = 10)),
+                 top =
+                 list(fun = dendrogramGrob,
+                      args =
+                      list(x = dd.row, 
+                           side = "top"))))
+}
+
